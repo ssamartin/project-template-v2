0264e5c72b346ac3797e4452952467e564e6ce5c
ssamartin <59620514+ssamartin@users.noreply.github.com>
2020-06-03 15:44:56 -0700

    changes

--- js/script.js
+++ js/script.js
@@ -1,8 +1,9 @@
 // your script file for adding your own jquery
 $(function() {
 // Your Code from here on down. Don't delete that line above!
-  
 
+$('.d2').mouseover(function(){ 	$('.firstnum').addClass('one') })
+$('.d2').mouseover(function(){ 	$('.secondnum').addClass('two') })
 
 
 
